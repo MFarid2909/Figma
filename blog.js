7857f1514bfef5c709541053eaffa97a60bd5d29
MFarid2909 <106157876+MFarid2909@users.noreply.github.com>
2022-10-11 19:17:40 +0400

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,341 @@
+<template>
+  <div>
+    <div class="blog-blog">
+      <div class="blog-group4518">
+        <img
+          src="/playground_assets/image42184-fvc-500h.png"
+          alt="image42184"
+          class="blog-image4"
+        />
+        <img
+          src="/playground_assets/image62185-l3y-400h.png"
+          alt="image62185"
+          class="blog-image6"
+        />
+        <img
+          src="/playground_assets/image72186-e01d-600h.png"
+          alt="image72186"
+          class="blog-image7"
+        />
+        <span class="blog-text"><span>Birhesab.az</span></span>
+        <div class="blog-group4392">
+          <span class="blog-text02"><span>Həllər</span></span>
+          <span class="blog-text04"><span>Məsləhət xidmətləri</span></span>
+          <span class="blog-text06"><span>Bloqlar</span></span>
+        </div>
+        <div class="blog-group4393">
+          <span class="blog-text08"><span>Daxil ol</span></span>
+          <div class="blog-btn">
+            <div class="blog-form01atomvalueplaceholder">
+              <span class="blog-text10"><span>Qeydiyyat</span></span>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/playground_assets/rectangle44482195-0mlq-200h.png"
+          alt="Rectangle44482195"
+          class="blog-rectangle4448"
+        />
+        <span class="blog-text12"><span>Az</span></span>
+        <img
+          src="/playground_assets/akariconschevrondown2197-ueff.svg"
+          alt="akariconschevrondown2197"
+          class="blog-akariconschevrondown"
+        />
+        <img
+          src="/playground_assets/rectangle44562199-ebob-200h.png"
+          alt="Rectangle44562199"
+          class="blog-rectangle4456"
+        />
+        <span class="blog-text14"><span>2022 Bütün hüquqlar qorunur</span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  props: {},
+}
+</script>
+
+<style scoped>
+.blog-blog {
+  width: 100%;
+  height: 1779px;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: transparent;
+  background-color: rgba(246, 255, 253, 1);
+}
+.blog-group4518 {
+  top: 0px;
+  left: 0px;
+  width: 1440px;
+  height: 1779px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  align-self: auto;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 1;
+  border-color: transparent;
+  border-style: none;
+  border-width: 0;
+  margin-right: 0;
+  border-radius: 0px 0px 0px 0px;
+  margin-bottom: 0;
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: transparent;
+}
+.blog-image4 {
+  top: 192px;
+  left: 0px;
+  width: 1440px;
+  height: 476px;
+  position: absolute;
+  border-color: transparent;
+}
+.blog-image6 {
+  top: 718px;
+  left: 104px;
+  width: 1263px;
+  height: 345px;
+  position: absolute;
+  border-color: transparent;
+}
+.blog-image7 {
+  top: 1113px;
+  left: 104px;
+  width: 1248px;
+  height: 553px;
+  position: absolute;
+  border-color: transparent;
+}
+.blog-text {
+  left: 134px;
+  color: rgba(38, 184, 147, 1);
+  width: 253px;
+  height: auto;
+  position: absolute;
+  font-size: 40px;
+  align-self: auto;
+  font-style: Bold;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-group4392 {
+  top: 27px;
+  left: 572px;
+  width: 291px;
+  height: 17px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  align-self: auto;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 1;
+  border-color: transparent;
+  border-style: none;
+  border-width: 0;
+  border-radius: 0px 0px 0px 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: transparent;
+}
+.blog-text02 {
+  color: rgba(0, 0, 0, 1);
+  width: 45px;
+  height: auto;
+  position: absolute;
+  font-size: 14px;
+  align-self: auto;
+  font-style: Regular;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-text04 {
+  left: 70px;
+  color: rgba(0, 0, 0, 1);
+  width: 144px;
+  height: auto;
+  position: absolute;
+  font-size: 14px;
+  align-self: auto;
+  font-style: Regular;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-text06 {
+  left: 239px;
+  color: rgba(0, 0, 0, 1);
+  width: 52px;
+  height: auto;
+  position: absolute;
+  font-size: 14px;
+  align-self: auto;
+  font-style: Regular;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-group4393 {
+  top: 19px;
+  left: 981px;
+  width: 224px;
+  height: 42px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  align-self: auto;
+  box-sizing: border-box;
+  align-items: flex-start;
+  flex-shrink: 1;
+  border-color: transparent;
+  border-style: none;
+  border-width: 0;
+  border-radius: 0px 0px 0px 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: transparent;
+}
+.blog-text08 {
+  top: 11px;
+  color: rgba(38, 184, 147, 1);
+  width: 54px;
+  height: auto;
+  position: absolute;
+  font-size: 14px;
+  align-self: auto;
+  font-style: Regular;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-btn {
+  top: 0px;
+  left: 96px;
+  width: 128px;
+  height: 42px;
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-color: rgba(38, 184, 147, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 8px;
+  background-color: var(--dl-color-secandery-one);
+}
+.blog-form01atomvalueplaceholder {
+  top: 11px;
+  left: 26.5px;
+  width: 76px;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  border-color: transparent;
+}
+.blog-text10 {
+  color: var(--dl-color-primary-tow);
+  height: auto;
+  font-size: 16px;
+  align-self: auto;
+  font-style: Bold;
+  text-align: center;
+  font-family: Nunito;
+  font-weight: 700;
+  line-height: 19.600000381469727px;
+  font-stretch: normal;
+  margin-right: 0;
+  margin-bottom: 0;
+  text-decoration: none;
+}
+.blog-rectangle4448 {
+  top: 27px;
+  left: 1248px;
+  width: 57px;
+  height: 26px;
+  position: absolute;
+  border-color: rgba(194, 194, 194, 1);
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 4px;
+}
+.blog-text12 {
+  top: 29px;
+  left: 1258px;
+  color: rgba(204, 204, 204, 1);
+  height: auto;
+  position: absolute;
+  font-size: 14px;
+  align-self: auto;
+  font-style: Bold;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.blog-akariconschevrondown {
+  top: 31px;
+  left: 1282px;
+  width: 18px;
+  height: 18px;
+  position: absolute;
+}
+.blog-rectangle4456 {
+  top: 1716px;
+  left: 0px;
+  width: 1440px;
+  height: 63px;
+  position: absolute;
+  border-color: rgba(38, 184, 147, 1);
+  border-style: solid;
+  border-width: 1px;
+}
+.blog-text14 {
+  top: 1734px;
+  left: 567px;
+  color: rgba(0, 0, 0, 1);
+  height: auto;
+  position: absolute;
+  font-size: 23px;
+  align-self: auto;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) ;
+  font-style: Bold;
+  text-align: left;
+  font-family: Roboto;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+</style>
