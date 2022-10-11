dde025805df838f3bd505de13db05865174e7f9e
MFarid2909 <106157876+MFarid2909@users.noreply.github.com>
2022-10-11 19:05:21 +0400

    Create landing-page.js

---
+++ landing-page.js
@@ -0,0 +1,368 @@
+//React kodu
+
+import React from 'react'
+
+import styles from './landing-page.module.css'
+
+const LandingPage = (props) => {
+  return (
+    <div>
+      <div className={styles['landing-page']}>
+        <span className={styles['text']}>
+          <span>Birhesab.az</span>
+        </span>
+        <div className={styles['group4392']}>
+          <span className={styles['text002']}>
+            <span>Həllər</span>
+          </span>
+          <span className={styles['text004']}>
+            <span>Məsləhət xidmətləri</span>
+          </span>
+          <span className={styles['text006']}>
+            <span>Bloqlar</span>
+          </span>
+        </div>
+        <div className={styles['group4393']}>
+          <span className={styles['text008']}>
+            <span>Daxil ol</span>
+          </span>
+          <div className={styles['btn']}>
+            <div className={styles['form01atomvalueplaceholder']}>
+              <span className={styles['text010']}>
+                <span>Qeydiyyat</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/playground_assets/imagesassehife1123-oeg-500h.png"
+          alt="IMAGEsassehife1123"
+          className={styles['i-m-a-g-esassehife1']}
+        />
+        <span className={styles['text012']}>
+          <span className={styles['text013']}>Birhesab</span>
+          <span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>Mikro sahibkarlarının biznes assistenti</span>
+            <br></br>
+            <span></span>
+          </span>
+        </span>
+        <div className={styles['group4517']}>
+          <img
+            src="/playground_assets/rectangle4442126-9k88-200h.png"
+            alt="Rectangle4442126"
+            className={styles['rectangle4442']}
+          />
+          <img
+            src="/playground_assets/rectangle4443127-6dq-200h.png"
+            alt="Rectangle4443127"
+            className={styles['rectangle4443']}
+          />
+          <img
+            src="/playground_assets/rectangle4444128-hutn-200h.png"
+            alt="Rectangle4444128"
+            className={styles['rectangle4444']}
+          />
+          <img
+            src="/playground_assets/rectangle4445129-n7x3-200h.png"
+            alt="Rectangle4445129"
+            className={styles['rectangle4445']}
+          />
+          <span className={styles['text020']}>
+            <span>
+              <span>Vergi Ödəyicisi ol</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text025']}>
+            <span>
+              <span>Hesabatını Göndər</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text030']}>
+            <span>
+              <span>Gəlir və Uçotunu qeydə al</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text035']}>
+            <span>
+              <span>Sənədlər yarat</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+        </div>
+        <img
+          src="/playground_assets/rectangle4446134-k82i-500h.png"
+          alt="Rectangle4446134"
+          className={styles['rectangle4446']}
+        />
+        <span className={styles['text040']}>
+          <span>Vergi ödəyicisi olmaq istəyirsiniz?</span>
+        </span>
+        <span className={styles['text042']}>
+          <span>
+            <span>Şirkət açmaq istəyirsiniz?</span>
+            <br></br>
+            <span></span>
+          </span>
+        </span>
+        <img
+          src="/playground_assets/rectangle4448137-tfga-200h.png"
+          alt="Rectangle4448137"
+          className={styles['rectangle4448']}
+        />
+        <span className={styles['text047']}>
+          <span>Az</span>
+        </span>
+        <img
+          src="/playground_assets/akariconschevrondown139-urxa.svg"
+          alt="akariconschevrondown139"
+          className={styles['akariconschevrondown']}
+        />
+        <img
+          src="/playground_assets/rectangle4449141-9x2e-200h.png"
+          alt="Rectangle4449141"
+          className={styles['rectangle4449']}
+        />
+        <span className={styles['text049']}>
+          <span>Qeydiyyat</span>
+        </span>
+        <div className={styles['group3']}>
+          <img
+            src="/playground_assets/sandtimer145-7pu-200h.png"
+            alt="SandTimer145"
+            className={styles['sand-timer']}
+          />
+          <div className={styles['component1']}>
+            <img
+              src="/playground_assets/rectangle18147-24a-200h.png"
+              alt="Rectangle18147"
+              className={styles['rectangle18']}
+            />
+            <img
+              src="/playground_assets/rectangle16148-uh3p-200h.png"
+              alt="Rectangle16148"
+              className={styles['rectangle16']}
+            />
+            <img
+              src="/playground_assets/statistics149-djcc-200h.png"
+              alt="Statistics149"
+              className={styles['statistics']}
+            />
+          </div>
+          <img
+            src="/playground_assets/rectangle4451150-z44-200h.png"
+            alt="Rectangle4451150"
+            className={styles['rectangle4451']}
+          />
+          <span className={styles['text051']}>
+            <span>Qeydiyyat</span>
+          </span>
+        </div>
+        <img
+          src="/playground_assets/rectangle5152-zdip-400h.png"
+          alt="Rectangle5152"
+          className={styles['rectangle5']}
+        />
+        <img
+          src="/playground_assets/intersect153-8d7f.svg"
+          alt="Intersect153"
+          className={styles['intersect']}
+        />
+        <img
+          src="/playground_assets/intersect156-v8y.svg"
+          alt="Intersect156"
+          className={styles['intersect1']}
+        />
+        <span className={styles['text053']}>
+          <span>
+            <span>Fərdi sahibkar kimi hesabat göndərmək istəyirsiniz?</span>
+            <br></br>
+            <span></span>
+          </span>
+        </span>
+        <img
+          src="/playground_assets/vector160-g7jr.svg"
+          alt="Vector160"
+          className={styles['vector']}
+        />
+        <img
+          src="/playground_assets/vector161-bxl.svg"
+          alt="Vector161"
+          className={styles['vector1']}
+        />
+        <div className={styles['group4513']}>
+          <span className={styles['text058']}>
+            <span>Hesabata keç</span>
+          </span>
+          <img
+            src="/playground_assets/arrow27165-u36.svg"
+            alt="Arrow27165"
+            className={styles['arrow27']}
+          />
+        </div>
+        <span className={styles['text060']}>
+          <span>
+            <span>
+              Biznesinizin idarə olunması üçün gəlirlərinizi və xərclərinizi
+              daima izləməlisiniz
+            </span>
+            <br></br>
+            <span>Köhnə üsullara son verin.</span>
+            <br></br>
+            <span>Birhesabla uçotunuzu avtomatlaşdırın</span>
+            <br></br>
+            <span></span>
+          </span>
+        </span>
+        <span className={styles['text069']}>
+          <span className={styles['text070']}>
+            <span>Biznesiniz üçün fərdi həllərə ehtiyac duyursunuz?</span>
+            <br></br>
+            <span></span>
+            <br></br>
+            <span></span>
+          </span>
+        </span>
+        <img
+          src="/playground_assets/rectangle4456168-o0wo-200h.png"
+          alt="Rectangle4456168"
+          className={styles['rectangle4456']}
+        />
+        <span className={styles['text076']}>
+          <span>2022 Bütün hüquqlar qorunur</span>
+        </span>
+        <div className={styles['group4515']}>
+          <img
+            src="/playground_assets/rectangle14171-109k-300w.png"
+            alt="Rectangle14171"
+            className={styles['rectangle14']}
+          />
+          <div className={styles['montlyvisits']}>
+            <div className={styles['group']}></div>
+          </div>
+          <div className={styles['montlyvisits1']}>
+            <div className={styles['group1']}></div>
+          </div>
+          <div className={styles['montlyvisits2']}>
+            <div className={styles['group2']}></div>
+          </div>
+          <div className={styles['component5']}>
+            <img
+              src="/playground_assets/rectangle17182-j2cl-200h.png"
+              alt="Rectangle17182"
+              className={styles['rectangle17']}
+            />
+            <img
+              src="/playground_assets/accounting183-966-200h.png"
+              alt="accounting183"
+              className={styles['accounting']}
+            />
+          </div>
+          <div className={styles['component4']}>
+            <img
+              src="/playground_assets/rectangle17185-x7r-200h.png"
+              alt="Rectangle17185"
+              className={styles['rectangle171']}
+            />
+            <img
+              src="/playground_assets/team1186-pp1e-200h.png"
+              alt="team1186"
+              className={styles['team1']}
+            />
+          </div>
+          <div className={styles['component2']}>
+            <img
+              src="/playground_assets/rectangle17188-rzon-200h.png"
+              alt="Rectangle17188"
+              className={styles['rectangle172']}
+            />
+            <img
+              src="/playground_assets/socialmedia189-98ry-200h.png"
+              alt="socialmedia189"
+              className={styles['socialmedia']}
+            />
+          </div>
+          <div className={styles['component3']}>
+            <img
+              src="/playground_assets/rectangle17191-21lx-200h.png"
+              alt="Rectangle17191"
+              className={styles['rectangle173']}
+            />
+            <img
+              src="/playground_assets/business192-t0fw-200h.png"
+              alt="business192"
+              className={styles['business']}
+            />
+          </div>
+          <span className={styles['text078']}>
+            <span>
+              <span>Marketing Plan</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text083']}>
+            <span>
+              <span>Biznes Plan</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text088']}>
+            <span>
+              <span>Hesabatın Hazırlanması</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <span className={styles['text093']}>
+            <span>
+              <span>Biznes Məsləhəti</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+        </div>
+        <div className={styles['group4516']}>
+          <img
+            src="/playground_assets/rectangle4453198-8vi-200h.png"
+            alt="Rectangle4453198"
+            className={styles['rectangle4453']}
+          />
+          <span className={styles['text098']}>
+            <span>Əlaqə</span>
+          </span>
+          <span className={styles['text100']}>
+            <span>E-mail ünvanınızı daxil edin</span>
+          </span>
+          <img
+            src="/playground_assets/rectangle44541101-oxlw-200h.png"
+            alt="Rectangle44541101"
+            className={styles['rectangle4454']}
+          />
+          <span className={styles['text102']}>
+            <span>Göndər</span>
+          </span>
+        </div>
+        <div className={styles['frame33670']}></div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
